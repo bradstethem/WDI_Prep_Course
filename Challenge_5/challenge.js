23b836f12edec62814ab2d3356a29434b32dabd8
Brad Stethem <brad@Brads-MacBook-Pro.local>
2017-01-05 15:31:12 -0500

    Challenge 5 complete

---
+++ Challenge_5/challenge.js
@@ -0,0 +1,39 @@
+// Excercise 1
+var name = "Brad Stethem";
+console.log("Hello " + name);
+
+// Excercise 2
+var x = 1125;
+var answer = x % 2;
+console.log(answer);
+
+// Excercise 3
+var a = true;
+var b = false;
+var c = true;
+console.log(a || b || c);
+
+// Excercise 4
+var a = true;
+var b = false;
+var c = true;
+console.log(a && b && c);
+
+// Excercise 5
+var answer1 = 10 - 3 * 5/4 + 202 % 3;
+var answer2 = 10 - (3 * (5/4) + (202 % 3));
+console.log(answer1);
+console.log(answer2);
+
+// Excercise 6
+String(12);
+parseInt("112");
+parseFloat("1.12356");
+var x = parseInt("Hello World");
+console.log(x);
+
+//Diving Deeper
+console.log("I am a BrainStation student".search(/BrainStation/));
+console.log("I am a student.".search(/BrainStation/));
+console.log("I am a BrainStation student".replace(/BrainStation student/,"Junior Developer"));
+
